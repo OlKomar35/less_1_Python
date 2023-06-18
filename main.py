bc740f4373fd69ec1dacbf3972d8212368e3a8b2
Olga Komar <komar_ol@mail.ru>
2023-06-18 14:47:11 +0300

    added task 1

--- main.py
+++ main.py
@@ -4,3 +4,14 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) 
 
+number = input('Введите  трехзначное число a=')
+number = int(number)
+sum = 0
+
+if number >= 100 and number <= 999:
+    while number > 0:
+        sum += number % 10 
+        number//=10
+    print(f'сумма цифр введденного числа ={sum}')
+else:
+    print('Введино не трехзначное число')
