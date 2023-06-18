653714166296f7b614bcb9dbecc2e65dfab38498
Olga Komar <komar_ol@mail.ru>
2023-06-18 16:05:09 +0300

    added task without cycle

--- main.py
+++ main.py
@@ -6,12 +6,9 @@
 
 number = input('Введите  трехзначное число a=')
 number = int(number)
-sum = 0
 
 if number >= 100 and number <= 999:
-    while number > 0:
-        sum += number % 10 
-        number//=10
+    sum = number % 10 +(number//10)%10+(number//100)%10
     print(f'сумма цифр введденного числа ={sum}')
 else:
     print('Введино не трехзначное число')
